698a113683d6f5d743a663d360b8d00bb80060ed
ThanhLong34 <thanhlongedu0304@gmail.com>
2023-04-28 14:24:44 +0700

    App use morgan

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 const port = 3000;
+const app = express();
+
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
